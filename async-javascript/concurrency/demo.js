12527b7a09d182e8adeb58c5874afe7b43195057
josueahadi <ahadihjosue@gmail.com>
2024-06-29 17:04:44 +0200

    fix: error handling with try and catch

--- async-javascript/concurrency/demo.js
+++ async-javascript/concurrency/demo.js
@@ -1,33 +1,35 @@
 function fetchData(url) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const xhr = new XMLHttpRequest()
-            xhr.open('GET', url, true);
-            xhr.send();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(xhr.responseText)
-                    } else {
-                        reject(xhr.status);
-                    }
-                }
-            }
-
-        }, Math.random() * 2000); // Simulating asynchronous delay
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            resolve(xhr.responseText);
+          } else {
+            reject(new Error(`Request failed with status ${xhr.status}`));
+          }
+        }
+      };
+      xhr.send(); // Send the request
+    }, Math.random() * 2000); // Simulating asynchronous delay
+  });
 }
 
 async function getDataConcurrently() {
-    const promises = [
-        fetchData('https://jsonplaceholder.typicode.com/users/1'),
-        fetchData('https://jsonplaceholder.typicode.com/users/2'),
-        fetchData('https://jsonplaceholder.typicode.com/users/3')
-    ]
-
-    const results = await Promise.all(promises)
-        .then(results => results.forEach(result => console.log(`USER:\n${result}`)))
-        .catch(error => console.erorr(error.message))
+  const promises = [
+    fetchData('https://jsonplaceholder.typicode.com/users/1'),
+    fetchData('https://jsonplaceholder.typicode.com/users/2'),
+    fetchData('https://jsonplaceholder.typicode.com/users/3')
+  ];
+  
+  try {
+    const results = await Promise.all(promises);
+    console.log(results);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-getDataConcurrently()
+getDataConcurrently();
